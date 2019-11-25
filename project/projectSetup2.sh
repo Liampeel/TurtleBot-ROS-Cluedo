73737cc069cbfa27a6e030ae33d3bc63b2aa88e9
Kieron <sc16kh@leeds.ac.uk>
2019-11-25 15:53:01 +0000

    project setup shell scripts

---
+++ project/projectSetup2.sh
@@ -0,0 +1,3 @@
+source ../../../devel/setup.bash
+cd launch
+roslaunch simulated_localisation.launch map_file:=$HOME/modules/robotics/catkin_ws/src/group_project/project/example/map/project_map.yaml
