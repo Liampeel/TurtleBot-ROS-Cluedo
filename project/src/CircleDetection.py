a9162c3c8098fff43bce656b10563890b66048fe
Katie Mitchell <sc16km@leeds.ac.uk>
2019-11-29 10:48:57 +0000

    Robot now detects if green circle and moves into room if green

--- project/src/CircleDetection.py
+++ project/src/CircleDetection.py
@@ -23,70 +23,100 @@ class colourIdentifier():
         self.green_circle_detected = False
         self.red_detected = False
         self.red_circle_detected = False
+        self.centralised = False
         self.bridge = CvBridge()
+        self.stop = False
         self.image_sub = rospy.Subscriber("camera/rgb/image_raw", Image, self.callback)
 
-        while not (self.green_detected or self.red_detected):
-            self.desired_velocity.angular.z = 0.2
+        while True:
+            if(self.centralised and not self.green_circle_detected):
+                self.desired_velocity.linear.x = 0.1
+                self.desired_velocity.angular.z = 0
+            if(self.green_circle_detected):
+                self.desired_velocity.linear.x = 0
+                self.desired_velocity.angular.z = 0
+
+                #ADD CODE TO TAKE IMAGE CAPTURE
+
+                cv2.destroyAllWindows()
+
+                break;
+            if (not self.green_detected and not (self.green_circle_detected or self.centralised)):
+                self.desired_velocity.angular.z = 0.2
+
             self.pub.publish(self.desired_velocity)
-            if(self.green_detected == True or self.red_detected == True):
-            	self.desired_velocity.angular.z = 0
+
 
 
     def callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
+
         except CvBridgeError as e:
             print(e)
+
+        #Declare upper and lower bounds for red and green hsv values
         hsv_green_lower = np.array([40,10,10])
         hsv_green_upper = np.array([80,255,255])
         hsv_red_lower = np.array([0,50,50])
         hsv_red_upper = np.array([5,255,255])
+
+
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+        #image mask declarations
         image_mask_green = cv2.inRange(hsv,hsv_green_lower,hsv_green_upper)
         image_mask_red = cv2.inRange(hsv,hsv_red_lower,hsv_red_upper)
+
+        #Combined red and green mask
         red_green_mask = cv2.bitwise_or(image_mask_red,image_mask_green)
+
+        #The combined masked feed to display at runtime
         image_res = cv2.bitwise_and(cv_image,cv_image, mask= red_green_mask)
+
+        #Find contours in the masks
         contours_green, hierarchy = cv2.findContours(image_mask_green.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         contours_red, hierarchy = cv2.findContours(image_mask_red.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        #Use HoughCircles function to detect circles within the masked images.
         green_circle = cv2.HoughCircles(image_mask_green, cv.CV_HOUGH_GRADIENT, 80,100, param1 = 700, param2 = 600, minRadius = 1, maxRadius = 200)
         red_circle = cv2.HoughCircles(image_mask_red, cv.CV_HOUGH_GRADIENT, 100, 120, param1 = 150, param2 = 150)#, param1 = 500, param2 = 400, minRadius = 1, maxRadius = 200 )
 
+
         width = np.size(image_res, 1)
         centre = (width / 2)
+
+        #green circle detection
         if len(contours_green) > 0:
             cgreen = max(contours_green, key = cv2.contourArea)
             M = cv2.moments(cgreen)
-            cxgreen, cygreen = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-
+            cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
             if cv2.contourArea(cgreen) > 1000:
-                print(cv2.contourArea(cgreen))
                 self.green_detected = True
-                print("Green detected")
-                if cv2.contourArea(cgreen) > 8000:
-                    if green_circle is not None:
-                        print("Circle detected")
-                        COLOUR_GREEN = np.array([0,255,0])
-                        green_circle = np.uint16(np.around(green_circle))
-                        for i in green_circle[0, :]:
-                            centre = (i[0], i[1])
-                            cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
-                            radius = i[2]
-                            cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
-                        self.green_circle_detected = True
+                if((cx < centre + 10) and (cx > centre -10 )):
+                    self.centralised = True;
+
+                if cv2.contourArea(cgreen) > 8000 and green_circle is not None:
+                    COLOUR_GREEN = np.array([0,255,0])
+                    green_circle = np.uint16(np.around(green_circle))
+                    for i in green_circle[0, :]:
+                        centre = (i[0], i[1])
+                        cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
+                        radius = i[2]
+                        cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
+                    self.green_circle_detected = True
 
         else:
             self.green_detected = False
 
+        #Red circle detection
         if len(contours_red) > 0:
             cred = max(contours_red, key = cv2.contourArea)
             M = cv2.moments(cred)
-            cxred, cyred = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
             if cv2.contourArea(cred) > 1000:
                 print(cv2.contourArea(cred))
                 self.red_detected = True
-
                 print("Red detected")
                 if red_circle is not None and cv2.contourArea(cred) > 8000:
                     print("Circle detected")
@@ -98,21 +128,14 @@ class colourIdentifier():
                         radius = i[2]
                         cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
                     self.red_circle_detected = True
-                #cv2.circle(cv_image,(cx,cy), 50, COLOUR_RED, 1)
-                # cv2.circle(<image>,(<center x>,<center y>),<radius>,<colour (rgb tuple)>,<thickness (defaults to 1)>)
-                # Then alter the values of any flags
         else:
             self.red_detected = False
 
-
-
-
-
+        #Display feeds
         cv2.namedWindow('Camera_Feed')
         cv2.imshow("Camera_Feed", cv_image)
         cv2.imshow('res',image_res)
-        cv2.waitKey(3)
-
+        cv2.waitKey(1)
 
 
 def main(args):
--- project/src/main.py
+++ project/src/main.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     INPUT_POINTS_PATH = "../example/input_points.yaml"
     room_1 = RoomOne(INPUT_POINTS_PATH)
     room_2 = RoomTwo(INPUT_POINTS_PATH)
+    green = False
+    red = False
 
     try:
         rospy.init_node('move_to_point', anonymous=True)
@@ -35,15 +37,51 @@ if __name__ == '__main__':
 
             while True:
                 cI=colourIdentifier()
-                rospy.loginfo("HERE")
-                if cI.green_detected:
+                if cI.green_circle_detected:
                     rospy.loginfo("GREEN")
+                    green = True
+                    break
+                elif cI.red_circle_detected:
+                    rospy.loginfo("RED")
+                    red = True
                     break
             rospy.loginfo("DETECTED ONE")
-
-
         else:
             rospy.loginfo("The base failed to reach the desired pose")
+
+        if green:
+            
+            # Customize the following values so they are appropriate for your location
+            x = room_1.get_centre("x")
+            y = room_1.get_centre("y")
+            theta = 0  # This is for rotation
+            position = {'x': x, 'y': y}
+            quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta/2.0), 'r4': np.cos(theta/2.0)}
+
+            rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+            success = navigator.goto(position, quaternion)
+            if success:
+                rospy.loginfo("Hooray, reached the desired pose")
+            else:
+                rospy.loginfo("The base failed to reach the desired pose")
+
+        elif red:
+            rospy.init_node('move_to_point', anonymous=True)
+            navigator = GoToPose()
+
+            # Customize the following values so they are appropriate for your location
+            x = room_2.get_entrance("x")
+            y = room_2.get_entrance("y")
+            theta = 0  # This is for rotation
+            position = {'x': x, 'y': y}
+            quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta/2.0), 'r4': np.cos(theta/2.0)}
+
+            rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+            success = navigator.goto(position, quaternion)
+            if success:
+                rospy.loginfo("Hooray, reached the desired pose")
+            else:
+                rospy.loginfo("The base failed to reach the desired pose")
 
         # Sleep to give the last log messages time to be sent
         rospy.sleep(1)
