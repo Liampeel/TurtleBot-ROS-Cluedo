7fb99e112208c582d75cf97c0d6644e9a06cbeda
Kieron <sc16kh@leeds.ac.uk>
2019-12-06 16:27:13 +0000

    modularised and added comments to circle detection code

--- project/src/CircleDetection.py
+++ project/src/CircleDetection.py
@@ -15,6 +15,10 @@ from std_msgs.msg import String
 
 class colourIdentifier():
 
+
+
+
+
     def __init__(self):
 
         self.pub = rospy.Publisher('mobile_base/commands/velocity', Twist)
@@ -42,15 +46,91 @@ class colourIdentifier():
             self.pub.publish(self.desired_velocity)
 
 
+    #Return a masked image based on lower and upper colour bounds
+    def getColourMask(self,hsv, hsv_lower,hsv_upper):
+        mask = cv2.inRange(hsv,hsv_lower,hsv_upper)
+        return mask
 
+    #Return the combination of two masks using bitwise_or
+    def getCombinedMasks(self, mask1, mask2):
+        combinedMask = cv2.bitwise_or(mask1,mask2)
+        return combinedMask
+
+    #return the image of the combined mask to display
+    def getCominedMaskCamFeed(self, image, combinedMask):
+        camFeed = cv2.bitwise_and(image, image, mask = combinedMask)
+        return camFeed
+
+    #return contours of a mask
+    def getContours(self, mask):
+        contours , hierarchy = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        return contours, hierarchy
+
+    #return converted colour of an image
+    def convertColour(self, image, conversion):
+        newImage = cv2.cvtColor(image, conversion)
+        return newImage
+
+    #return the maximum contours
+    def getMaxContours(self, contours):
+        maxContours = max(contours, key = cv2.contourArea)
+        return maxContours
+
+    #detect if the area of the maximum contour is greater than 1000,
+    #if it is, we say that we have detected an object of that colour
+    #return a boolean that states if the colour has been DETECTED
+    #return the maximum contour
+    def detectColour(self,contours):
+        colourDetected = False;
+        maxContour = self.getMaxContours(contours)
+        if cv2.contourArea(maxContour) > 1000:
+            colourDetected = True
+        else:
+            colourDetected = False
+        return colourDetected, maxContour
+
+    #get the centre points of the coloured object based on the moments of
+    #the maximum contour
+    def getCentrePoints(self, maxContour):
+        M = cv2.moments(maxContour)
+        cx = 0
+        cy = 0
+        try:
+            cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+        except ZeroDivisionError:
+            pass
+        return cx, cy
+
+    #draw a circle over the outline of the found circle
+    def drawCircles(self,circles, cv_image):
+        COLOUR_GREEN = np.array([0,255,0])
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            centre = (i[0], i[1])
+            cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
+            radius = i[2]
+        cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
+
+    #check that we are central with the found object
+    def centralise(self, cx, cy, centre):
+        centralised = False
+        if((cx < centre + 30) and (cx > centre -30 )):
+            centralised = True
+
+    #detect if there is a circle in the image
+    def detectCircle(self, maxContour, circles, cv_image):
+        circle_detected = False
+        if cv2.contourArea(maxContour) > 8000 and circles is not None:
+            circle_detected = True
+            self.drawCircles(circles,cv_image)
+        return circle_detected
+
+    #run the circle detection code
     def callback(self, data):
-
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             if not self.detect:
                 return
-
-
         except CvBridgeError as e:
             print(e)
         except AttributeError:
@@ -62,94 +142,63 @@ class colourIdentifier():
         hsv_red_lower = np.array([0,50,50])
         hsv_red_upper = np.array([5,255,255])
 
-
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        #Set the hsv image
+        hsv = self.convertColour(cv_image, cv2.COLOR_BGR2HSV)
 
         #image mask declarations
-        image_mask_green = cv2.inRange(hsv,hsv_green_lower,hsv_green_upper)
-        image_mask_red = cv2.inRange(hsv,hsv_red_lower,hsv_red_upper)
+        image_mask_green = self.getColourMask(hsv, hsv_green_lower, hsv_green_upper)
+        image_mask_red = self.getColourMask(hsv, hsv_red_lower, hsv_red_upper)
 
         #Combined red and green mask
-        red_green_mask = cv2.bitwise_or(image_mask_red,image_mask_green)
+        red_green_mask = self.getCombinedMasks(image_mask_red, image_mask_green)
 
         #The combined masked feed to display at runtime
-        image_res = cv2.bitwise_and(cv_image,cv_image, mask= red_green_mask)
+        image_res = self.getCominedMaskCamFeed(cv_image, red_green_mask)
 
         #Find contours in the masks
-        contours_green, hierarchy = cv2.findContours(image_mask_green.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        contours_red, hierarchy = cv2.findContours(image_mask_red.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        contours_green, hierarchy = self.getContours(image_mask_green)
+        contours_red, hierarchy = self.getContours(image_mask_red)
+
+        #initialise greyscale image and blur it
+        grey = self.convertColour(cv_image, cv2.COLOR_BGR2GRAY)
         grey = cv2.medianBlur(grey,9)
+
         #Use HoughCircles function to detect circles within the masked images.
         circles = cv2.HoughCircles(grey, cv.CV_HOUGH_GRADIENT,1,20,param1 = 50, param2= 30,minRadius = 90,maxRadius = 110)
+
+        #finding the centre point of the combined mask
         width = np.size(image_res, 1)
         centre = (width / 2)
 
-
-
-
-        #green circle detection
+        #if we find any green contours
         if len(contours_green) > 0:
+            #call the detect colour function
+            self.green_detected, maxGreenContour = self.detectColour(contours_green)
+            #find centre points of the green object
+            cxGreen, cyGreen = self.getCentrePoints(maxGreenContour)
+            #check if we are central to the green object
+            self.centralised = self.centralise(cxGreen, cyGreen, centre)
+            if self.green_detected == True:
+                #if we have detected green, call the detect circle function to see if this is a green circle
+                self.green_circle_detected = self.detectCircle(maxGreenContour, circles, cv_image)
 
-            cgreen = max(contours_green, key = cv2.contourArea)
-            M = cv2.moments(cgreen)
-            try:
-                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-            except ZeroDivisionError:
-                pass
-            if cv2.contourArea(cgreen) > 1000:
-                self.green_detected = True
-                if((cx < centre + 30) and (cx > centre -30 )):
-                    self.centralised = True;
-
-                if cv2.contourArea(cgreen) > 8000 and circles is not None:
-                    COLOUR_GREEN = np.array([0,255,0])
-                    circles = np.uint16(np.around(circles))
-                    for i in circles[0, :]:
-                        centre = (i[0], i[1])
-                        cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
-                        radius = i[2]
-                    cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
-                    cv2.imwrite('green_circle.png', cv_image)
-                    self.green_circle_detected = True
-
-        else:
-            self.green_detected = False
-
-        #Red circle detection
-        #Red circle detection
+        #if we find any red contours
         if len(contours_red) > 0:
-            cred = max(contours_red, key = cv2.contourArea)
-            M = cv2.moments(cred)
-            try:
-                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-            except ZeroDivisionError:
-                pass
+            #call the detecxt colour function
+            self.red_detected, maxRedContour = self.detectColour(contours_red)
+            #find the centre points of the found red object
+            cxRed, cyRed = self.getCentrePoints(maxRedContour)
+            #check if we are central to the red object
+            self.centralised = self.centralise(cxRed, cyRed, centre)
+            if self.red_detected == True:
+                #if we have detected red, call the detect circle function to see if this object is a red circle
+                self.red_circle_detected = self.detectCircle(maxRedContour, circles, cv_image)
 
-            if cv2.contourArea(cred) > 1000:
-                self.red_detected = True
-                if((cx < centre + 30) and (cx > centre -30 )):
-                    self.centralised = True;
-
-                if circles is not None and cv2.contourArea(cred) > 8000:
-                    COLOUR_GREEN = np.array([0,255,0])
-                    circles = np.uint16(np.around(circles))
-                    for i in circles[0, :]:
-                        centre = (i[0], i[1])
-                        cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
-                        radius = i[2]
-                    cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
-                    self.red_circle_detected = True
-        else:
-            self.red_detected = False
-
-
-
-        #Display feeds
-        #cv2.namedWindow('Camera_Feed')
-        #cv2.imshow("Camera_Feed", cv_image)
-        #cv2.imshow('res',image_res)
-    #    cv2.waitKey(1)
+        # # Display feeds
+        # cv2.namedWindow('Camera_Feed')
+        # cv2.imshow("Camera_Feed", cv_image)
+        # cv2.imshow('res',image_res)
+        # cv2.waitKey(1)
 
 
 def main(args):
