979c0dca4e4f93f103f0f404b5b55dd57abd1bc7
John Lack-Wilson <sc16jlw@leeds.ac.uk>
2019-12-11 17:29:18 +0000

    Fixing a bug where the robot would sometimes take multiple screenshots. Cleaning code out into function

--- project/src/CircleDetection.py
+++ project/src/CircleDetection.py
@@ -9,14 +9,12 @@ import sys
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import String
-# import the necessary packages
 
 
 class colourIdentifier():
 
     def __init__(self):
-        self.pub = rospy.Publisher('mobile_base/commands/velocity', Twist)
+        self.pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=1)
         self.desired_velocity = Twist()
         self.green_detected = False
         self.green_circle_detected = False
@@ -28,111 +26,113 @@ class colourIdentifier():
         self.image_sub = rospy.Subscriber("camera/rgb/image_raw", Image, self.callback)
         self.detect = True
         while True:
-            if(self.centralised and not (self.green_circle_detected or self.red_circle_detected)):
+            if self.centralised and not (self.green_circle_detected or self.red_circle_detected):
                 self.desired_velocity.linear.x = 0.1
                 self.desired_velocity.angular.z = 0
-            if(self.green_circle_detected or self.red_circle_detected):
+            if self.green_circle_detected or self.red_circle_detected:
                 self.desired_velocity.linear.x = 0
                 self.desired_velocity.angular.z = 0
-                break;
-            if (not (self.green_detected or self.red_detected) and not (self.green_circle_detected or self.red_circle_detected or self.centralised)):
+                break
+            if not (self.green_detected or self.red_detected) and not (self.green_circle_detected or self.red_circle_detected or self.centralised):
                 self.desired_velocity.angular.z = 0.2
 
             self.pub.publish(self.desired_velocity)
 
-
-
     def callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             if not self.detect:
                 return
+
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
+            # Declare upper and lower bounds for red and green hsv values
+            hsv_green_lower = np.array([40, 10, 10])
+            hsv_green_upper = np.array([80, 255, 255])
+            hsv_red_lower = np.array([0, 50, 50])
+            hsv_red_upper = np.array([5, 255, 255])
+
+            hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+            # image mask declarations
+            image_mask_green = cv2.inRange(hsv, hsv_green_lower, hsv_green_upper)
+            image_mask_red = cv2.inRange(hsv, hsv_red_lower, hsv_red_upper)
+
+            # Combined red and green mask
+            red_green_mask = cv2.bitwise_or(image_mask_red, image_mask_green)
+
+            # The combined masked feed to display at runtime
+            image_res = cv2.bitwise_and(cv_image, cv_image, mask=red_green_mask)
+
+            # Find contours in the masks
+            contours_green, hierarchy = cv2.findContours(image_mask_green.copy(), cv2.RETR_LIST,
+                                                         cv2.CHAIN_APPROX_SIMPLE)
+            contours_red, hierarchy = cv2.findContours(image_mask_red.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            grey = cv2.medianBlur(grey, 9)
+            # Use HoughCircles function to detect circles within the masked images.
+            circles = cv2.HoughCircles(grey, cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=90,
+                                       maxRadius=110)
+            width = np.size(image_res, 1)
+            centre = (width / 2)
+
+            # green circle detection
+            if len(contours_green) > 0:
+                cgreen = max(contours_green, key=cv2.contourArea)
+                M = cv2.moments(cgreen)
+                try:
+                    cx, cy = int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])
+                except ZeroDivisionError:
+                    pass
+                if cv2.contourArea(cgreen) > 1000:
+                    self.green_detected = True
+                    if cx < centre + 30 and (cx > centre - 30):
+                        self.centralised = True
+
+                    if cv2.contourArea(cgreen) > 8000 and circles is not None:
+                        COLOUR_GREEN = np.array([0, 255, 0])
+                        circles = np.uint16(np.around(circles))
+                        for i in circles[0, :]:
+                            centre = (i[0], i[1])
+                            cv2.circle(cv_image, centre, 1, COLOUR_GREEN, 3)
+                            radius = i[2]
+                        cv2.circle(cv_image, centre, radius, COLOUR_GREEN, 3)
+                        rospy.loginfo("PICTURE")
+                        cv2.imwrite('green_circle.png', cv_image)
+                        self.green_circle_detected = True
+                        self.detect = False
+
+            else:
+                self.green_detected = False
+
+            # Red circle detection
+            if len(contours_red) > 0:
+                cred = max(contours_red, key=cv2.contourArea)
+                M = cv2.moments(cred)
+                try:
+                    cx, cy = int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])
+                except ZeroDivisionError:
+                    pass
+
+                if cv2.contourArea(cred) > 1000:
+                    self.red_detected = True
+                    if cx < centre + 30 and (cx > centre - 30):
+                        self.centralised = True
+                    if circles is not None and cv2.contourArea(cred) > 8000:
+                        COLOUR_GREEN = np.array([0, 255, 0])
+                        circles = np.uint16(np.around(circles))
+                        for i in circles[0, :]:
+                            centre = (i[0], i[1])
+                            cv2.circle(cv_image, centre, 1, COLOUR_GREEN, 3)
+                            radius = i[2]
+                        cv2.circle(cv_image, centre, radius, COLOUR_GREEN, 3)
+                        self.red_circle_detected = True
+            else:
+                self.red_detected = False
 
         except CvBridgeError as e:
             print(e)
         except AttributeError:
             pass
-
-        #Declare upper and lower bounds for red and green hsv values
-        hsv_green_lower = np.array([40,10,10])
-        hsv_green_upper = np.array([80,255,255])
-        hsv_red_lower = np.array([0,50,50])
-        hsv_red_upper = np.array([5,255,255])
-
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-
-        #image mask declarations
-        image_mask_green = cv2.inRange(hsv,hsv_green_lower,hsv_green_upper)
-        image_mask_red = cv2.inRange(hsv,hsv_red_lower,hsv_red_upper)
-
-        #Combined red and green mask
-        red_green_mask = cv2.bitwise_or(image_mask_red,image_mask_green)
-
-        #The combined masked feed to display at runtime
-        image_res = cv2.bitwise_and(cv_image,cv_image, mask= red_green_mask)
-
-        #Find contours in the masks
-        contours_green, hierarchy = cv2.findContours(image_mask_green.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        contours_red, hierarchy = cv2.findContours(image_mask_red.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        grey = cv2.medianBlur(grey,9)
-        #Use HoughCircles function to detect circles within the masked images.
-        circles = cv2.HoughCircles(grey, cv.CV_HOUGH_GRADIENT,1,20,param1 = 50, param2= 30,minRadius = 90,maxRadius = 110)
-        width = np.size(image_res, 1)
-        centre = (width / 2)
-
-        #green circle detection
-        if len(contours_green) > 0:
-            cgreen = max(contours_green, key = cv2.contourArea)
-            M = cv2.moments(cgreen)
-            try:
-                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-            except ZeroDivisionError:
-                pass
-            if cv2.contourArea(cgreen) > 1000:
-                self.green_detected = True
-                if((cx < centre + 30) and (cx > centre -30 )):
-                    self.centralised = True;
-
-                if cv2.contourArea(cgreen) > 8000 and circles is not None:
-                    COLOUR_GREEN = np.array([0,255,0])
-                    circles = np.uint16(np.around(circles))
-                    for i in circles[0, :]:
-                        centre = (i[0], i[1])
-                        cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
-                        radius = i[2]
-                    cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
-                    cv2.imwrite('green_circle.png', cv_image)
-                    self.green_circle_detected = True
-
-        else:
-            self.green_detected = False
-
-        #Red circle detection
-        if len(contours_red) > 0:
-            cred = max(contours_red, key = cv2.contourArea)
-            M = cv2.moments(cred)
-            try:
-                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-            except ZeroDivisionError:
-                pass
-
-            if cv2.contourArea(cred) > 1000:
-                self.red_detected = True
-                if((cx < centre + 30) and (cx > centre -30 )):
-                    self.centralised = True;
-
-                if circles is not None and cv2.contourArea(cred) > 8000:
-                    COLOUR_GREEN = np.array([0,255,0])
-                    circles = np.uint16(np.around(circles))
-                    for i in circles[0, :]:
-                        centre = (i[0], i[1])
-                        cv2.circle(cv_image, centre, 1, COLOUR_GREEN,3)
-                        radius = i[2]
-                    cv2.circle(cv_image,centre,radius,COLOUR_GREEN,3)
-                    self.red_circle_detected = True
-        else:
-            self.red_detected = False
 
 
 def main(args):
--- project/src/main.py
+++ project/src/main.py
@@ -9,40 +9,30 @@ from cluedofinder import CluedoFinder
 from CharacterDetection import characterDetection
 import cv2
 
+green = False
+red = False
+successMove = False
+navigator = None
+room_1 = None
+room_2 = None
 
-def moveBasedOnRedOrGreen(green, red, navigator):
-    success = False
-    if green:
 
-        # Customize the following values so they are appropriate for your location
-        x = room_1.get_centre("x")
-        y = room_1.get_centre("y")
-        theta = 0  # This is for rotation
-        position = {'x': x, 'y': y}
-        quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta/2.0), 'r4': np.cos(theta/2.0)}
+def moveBasedOnRedOrGreen(location, room):
+    global navigator
 
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-        else:
-            rospy.loginfo("The base failed to reach the desired pose")
+    if location == "entrance":
+        x = room.get_entrance("x")
+        y = room.get_entrance("y")
+    else:
+        x = room.get_centre("x")
+        y = room.get_centre("y")
 
-    elif red:
-        rospy.init_node('move_to_point', anonymous=True)
-        navigator = GoToPose()
+    theta = 0  # This is for rotation
+    position = {'x': x, 'y': y}
+    quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta / 2.0), 'r4': np.cos(theta / 2.0)}
 
-        # Customize the following values so they are appropriate for your location
-        x = room_2.get_entrance("x")
-        y = room_2.get_entrance("y")
-        theta = 0  # This is for rotation
-        position = {'x': x, 'y': y}
-        quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta/2.0), 'r4': np.cos(theta/2.0)}
-
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-
-    return success
+    rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+    return navigator.goto(position, quaternion)
 
 
 if __name__ == '__main__':
@@ -56,75 +46,31 @@ if __name__ == '__main__':
     INPUT_POINTS_PATH = sys.argv[1]
     room_1 = RoomOne(INPUT_POINTS_PATH)
     room_2 = RoomTwo(INPUT_POINTS_PATH)
-    green = False
-    red = False
-
-    success = False
-    successGreen = False
-    successMove = False
 
     try:
         rospy.init_node('move_to_point', anonymous=True)
         navigator = GoToPose()
 
-        # Customize the following values so they are appropriate for your location
-        x = room_1.get_entrance("x")
-        y = room_1.get_entrance("y")
-        theta = 0  # This is for rotation
-        position = {'x': x, 'y': y}
-        quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': np.sin(theta/2.0), 'r4': np.cos(theta/2.0)}
+        room = room_1
+        if moveBasedOnRedOrGreen("entrance", room):
+            cI = colourIdentifier()
+            while True:
+                if cI.red_circle_detected:
+                    rospy.loginfo("RED")
+                    red = True
+                    room = room_2
+                    cI = None
 
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-            try:
-                while True:
-                    cI=colourIdentifier()
-                    if cI.green_circle_detected:
+                if moveBasedOnRedOrGreen("entrance", room):
+                    cI2 = colourIdentifier()
+                    if cI2.green_circle_detected:
                         rospy.loginfo("GREEN")
                         green = True
-                        cI.detect = False
                         break
-                    elif cI.red_circle_detected:
-                        rospy.loginfo("RED")
-                        red = True
-                        cI.detect = False
-                        break
-            except KeyboardInterrupt:
-                exit(-1)
-            rospy.loginfo("DETECTED ONE")
         else:
-            rospy.loginfo("The base failed to reach the desired pose")
+            exit(0)
 
-        successMove = moveBasedOnRedOrGreen(green, red, navigator)
-        if(red and successMove):
-            red = False
-            green = False
-            try:
-                while True:
-                    cI=colourIdentifier()
-                    if cI.green_circle_detected:
-                        rospy.loginfo("GREEN")
-                        green = True
-                        cI.detect = False
-                        break
-                    elif cI.red_circle_detected:
-                        rospy.loginfo("RED")
-                        red = True
-                        cI.detect = False
-                        break
-            except KeyboardInterrupt:
-                exit(-1)
-            rospy.loginfo("DETECTED ONE")
-
-            successGreen = moveBasedOnRedOrGreen(green,red, navigator)
-
-        if(green and successMove):
-            successGreen = True
-
-        if(successGreen):
+        if green and moveBasedOnRedOrGreen("centre", room):
             try:
                 while True:
                     cF = CluedoFinder()
